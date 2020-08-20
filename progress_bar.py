2da2a902a1a432cc046fe325072002d6fc42af82
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-08-21 00:28:22 +0100

    Changed main progress bar feature to work better with different numbers and not mess up

--- progress_bar.py
+++ progress_bar.py
@@ -13,36 +13,36 @@ class ProgressBar:
         """
         self.max_count = max_count
         self.count = 0
+        self.padding = padding
         y_pos = MAX_ROWS - n
-        x_pos = 0 + padding
-        self.cols = MAX_COLS - 1 - 2 * padding
+        x_pos = 0 + self.padding
+        self.cols = MAX_COLS - 1 - 2 * self.padding
         self.mc_len = len(str(self.max_count))
-        self.perc = int((self.cols - 5 - 2 * self.mc_len) / self.max_count)
+        self.p_cols = self.cols - 5 - 2 * self.mc_len
+        self.perc = int(self.p_cols / self.max_count)
+        self.incre = 0
         self.window = curses.newwin(1, self.cols, y_pos, x_pos)
         self.move(True)
 
     def move(self, first=False):
         if not first:
             self.count += 1
+        self.incre = int(self.p_cols / self.max_count * self.count)
+
         if self.count == self.max_count:
-            line = "[%s%s=%s] %d/%d%s" \
-                   % ('=' * (int(self.max_count * self.perc / 2) - 4),
+            line = "[%s%s%s] %d/%d" \
+                   % ('=' * (int(self.incre / 2) - 4),
                       'COMPLETE',
-                      '=' * (int(self.max_count * self.perc / 2) - 4),
+                      '=' * (int(self.incre / 2) - 4),
                       self.count,
-                      self.max_count,
-                      ' ' * (self.cols - (int(self.count * self.perc) +
-                                          int((self.max_count - self.count) * self.perc)) - 6 -
-                             2*self.mc_len))
+                      self.max_count)
 
         else:
-            line = "[%s>%s] {number:0{width}d}/%d%s".format(width=self.mc_len, number=self.count)\
-                   % ('=' * int(self.count * self.perc),
-                      ' ' * int((self.max_count - self.count) * self.perc),
-                      self.max_count,
-                      ' ' * (self.cols - (int(self.count * self.perc) +
-                                          int((self.max_count - self.count) * self.perc)) - 6 -
-                             2*self.mc_len))
+            line = "[%s>%s] {number:0{width}d}/%d".format(width=self.mc_len, number=self.count) \
+                   % ('=' * self.incre,
+                      ' ' * (self.p_cols - self.incre - 1),
+                      self.max_count)
+        line += ' ' * (self.cols - len(line) - 1)
 
         self.print(line)
 
